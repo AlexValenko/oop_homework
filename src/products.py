e047101a91bfc6b3b5ea9454a575bc79b8e47bea
Alex Valenko <valenko.a.geo@gmail.com>
2026-06-22 22:07:55 +0300

    updated classes in product.py

--- src/products.py
+++ src/products.py
@@ -6,12 +6,53 @@ class Product:
     price: float
     quantity: int
 
+    # Список для хранения всех инициализированных товаров
+    prod_list = []
+
     def __init__(self, name: str, description: str, price: float, quantity: int):
         """Метод для инициализации экземпляров класса Product - все атрибуты задаются при инициализации"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+        Product.prod_list.append(self)
+
+    @classmethod
+    def new_product(cls, prod_dict):
+        """Метод принимает на вход словарь, в котором ключи соответствуют атрибутам экземпляра класса. Создает экземпляр класса.
+        Метод выполняет проверку имеются ли экземпляры с совпадающим полем name. Если продукт с таким именем существует, то
+        в существующем экземпляре добавляется количество товаров и актуализируется цена (берется наибольшая).
+        Если продукта с таким именем нет, то создается новый экземпляр класса (Продукт)"""
+        new_prod = cls(name=prod_dict.get("name"), description=prod_dict.get("description"), price=prod_dict.get("price"), quantity=prod_dict.get("quantity"))
+        for prod in cls.prod_list:
+            if prod.name == new_prod.name:
+                prod.quantity += new_prod.quantity
+                if new_prod.price > prod.price:
+                    prod.price = new_prod.price
+                return prod
+        cls.prod_list.append(new_prod)
+        return new_prod
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price > 0:
+            if new_price < self.__price:
+                print("Вы уверены, что хотите снизить цену?")
+                user_verifications = input('Да - введите "y", нет - любой непустой ввод \n').strip().lower()
+                if user_verifications == "y":
+                    self.__price = new_price
+                else:
+                    print("Цена не была изменена")
+
+            self.__price = new_price
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
+
+
 
 
 class Category:
@@ -29,6 +70,23 @@ class Category:
         Атрибуты класса: количество категорий и количество товаров обновляются при новой инициализации"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products)
+
+    # @property
+    # def products(self) -> list:
+    #     return self.__products
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        result = ''
+        for prod in self.__products:
+            result += f'{prod.name}, {prod.price} руб. Остаток: {prod.quantity} шт.\n'
+        return result
+
+
