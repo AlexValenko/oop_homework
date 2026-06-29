576527c47bae68f9fcd76826668a0d1ebe88d40e
Alex Valenko <valenko.a.geo@gmail.com>
2026-06-29 09:58:12 +0300

    task 1 done, added exception ValueError

--- tests/test_lawngrass.py
+++ tests/test_lawngrass.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.lawngrass import LawnGrass
 from src.products import Product
 
@@ -14,3 +16,9 @@ def test_smartphone_init(get_one_lawngrass) -> None:
     assert grass1.germination_period == "7 дней"
     assert grass1.color == "Зеленый"
     assert issubclass(LawnGrass, Product)
+
+def test_lawngrass_init_quantity_below_zero() -> None:
+    """Тестирование инициализации объекта класса Smartphone c нулевым количеством"""
+    with pytest.raises(ValueError) as exc_info:
+        LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 0, "Россия", "7 дней", "Зеленый")
+    assert str(exc_info.value) == "Товар с нулевым количеством не может быть добавлен"
